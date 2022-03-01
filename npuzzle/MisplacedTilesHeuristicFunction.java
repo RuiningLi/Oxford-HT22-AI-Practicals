ba6f6b17a4dae34c8745257809a8e912f3034308
RuiningLi <RayLibhsf@outlook.com>
2022-03-01 09:39:40 +0000

    Completed Practical 2 Task 3.

--- npuzzle/MisplacedTilesHeuristicFunction.java
+++ npuzzle/MisplacedTilesHeuristicFunction.java
@@ -6,6 +6,14 @@ import search.NodeFunction;
 public class MisplacedTilesHeuristicFunction implements NodeFunction {
     @Override
     public int getValue(Node n) {
-        return 0;
+        Tiles tiles = (Tiles)n.state;
+        int numTiles = tiles.width * tiles.width - 1;
+        int numMisplacedTiles = 0;
+        for (int index = 0; index <= numTiles; index++) {
+            if (tiles.tiles[index] != 0 && tiles.tiles[index] != (index + 1) % (numTiles + 1)) {
+                numMisplacedTiles++;
+            }
+        }
+        return numMisplacedTiles;
     }
 }
